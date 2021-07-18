c7a562583a3a794c3a8bd4704a48c378040c17a4
mehdi mohammadi <moh.meh41@gmail.com>
2021-07-18 16:20:37 +0430

    i18 deleted

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 const path = require("path");
-const { i18n } = require('./next-i18next.config')
+
 
     module.exports = {
-        i18n: {
-            defaultLocale: 'en',
-            locales: ['en', 'fa'],
-            defaultLanguage: 'en',
-            fallbackLng: ['en'],
-          },
+        
         images: {
             domains: ['https://res.cloudinary.com']
         },
